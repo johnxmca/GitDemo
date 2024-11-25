efb9f8081022f46ccf5498a56e822920c11264ac
johnxmca <tojohnx@gmail.com>
2024-11-25 21:03:37 +0530

    tst

---
+++ src/test/java/TestRunner/Abc.java
@@ -0,0 +1,14 @@
+package TestRunner;
+
+import org.testng.annotations.Test;
+
+
+public class Abc {
+
+    @Test
+    public void AA()
+    {
+        System.out.println("Kreupasanam Amma");
+    }
+
+}
